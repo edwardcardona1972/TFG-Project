38f89e5ca1ed5d19b5200b99deb54cdea4b79c5c
eduard Alexi cardona Grajales <edwardcardona1972@gmail.com>
2025-03-18 20:15:00 +0100

    Uso de los tabs para el listado de los personajes

--- TFG-Proyect/MVVM/View/ViewModel/DescripcionPersonajeViewController.swift
+++ TFG-Proyect/MVVM/View/ViewModel/DescripcionPersonajeViewController.swift
@@ -15,9 +15,10 @@ class DescripcionPersonajeViewController: UIViewController {
     @IBOutlet weak var tv: UITableView!
     @IBOutlet weak var mySegmentedControl: UISegmentedControl!
     
+    
     var character: CharacterModel?
-
     let listType: [String] = ["Cómics", "Historias", "Eventos", "Series"]
+    var selectedTypeIndex: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,6 +38,7 @@ class DescripcionPersonajeViewController: UIViewController {
         for (index, value) in listType.enumerated() {
             mySegmentedControl.setTitle(value, forSegmentAt: index)
         }
+       
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -52,20 +54,49 @@ class DescripcionPersonajeViewController: UIViewController {
     }
     
     @IBAction func mySegmentControlAction(_ sender: Any) {
-        print("Gracias hijo por tu paciencia con el ser que tanto te ama")
+        //3
+        selectedTypeIndex = mySegmentedControl.selectedSegmentIndex
+        tv.reloadData()
     }
 }
 
 extension DescripcionPersonajeViewController : UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.character?.comics?.items.count ?? 0
+        switch selectedTypeIndex {
+        case 1 :
+            return character?.stories?.items.count ?? 0
+        case 2 :
+            return character?.events?.items.count ?? 0
+        case 3 :
+            return character?.series?.items.count ?? 0
+        default:
+            return character?.comics?.items.count ?? 0
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let personaje = tableView.dequeueReusableCell(withIdentifier: "celda", for: indexPath)
-        personaje.textLabel?.text = character?.comics?.items[indexPath.row]?.name
-        personaje.detailTextLabel?.text = character?.comics?.items[indexPath.row]?.resourceURI
+        //5
+        switch selectedTypeIndex {
+        case 1 :
+            personaje.textLabel?.text = character?.stories?.items[indexPath.row]?.name
+            personaje.detailTextLabel?.text = character?.stories?.items[indexPath.row]?.resourceURI
+
+        case 2 :
+            personaje.textLabel?.text = character?.events?.items[indexPath.row]?.name
+            personaje.detailTextLabel?.text = character?.events?.items[indexPath.row]?.resurceURI
+            
+        case 3 :
+            personaje.textLabel?.text = character?.series?.items[indexPath.row]?.name
+            personaje.detailTextLabel?.text = character?.series?.items[indexPath.row]?.resourceURI
+            
+        default:
+            personaje.textLabel?.text = character?.comics?.items[indexPath.row]?.name
+            personaje.detailTextLabel?.text = character?.comics?.items[indexPath.row]?.resourceURI
+        }
+
         return personaje
     }
 }
+
